b971e1b07c1e3abb96bec8d54562f9f3e79cb266
Delyana <delyana28@abv.bg>
2023-02-10 12:15:49 +0200

    Initial Commit

---
+++ Consultant/ConsultantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsultantApp.swift
+//  Consultant
+//
+//  Created by Delyana Raykova on 10.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ConsultantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
